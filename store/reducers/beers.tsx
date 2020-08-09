f82ca909f955094885852dfef839513222a092f0
Pawel Bala <pawel3ala@gmail.com>
2020-08-09 16:15:14 +0200

    pagination initial version

--- store/reducers/beers.tsx
+++ store/reducers/beers.tsx
@@ -1,27 +1,52 @@
 import { BeerItem } from '../../types'
 import axios from 'axios'
 import { Dispatch } from 'redux'
+import { store } from '../configureStore';
 
+const pagesPerPage: number = 12;
 const beersReducerDefaultState: BeerItem[] = [];
+
 const SET_ALL_BEERS = 'SET_ALL_BEERS'
+const SET_MORE_BEERS = 'SET_MORE_BEERS'
 
 interface SetAllBeersAction {
   type: typeof SET_ALL_BEERS,
   beers: BeerItem[]
 }
 
-type BeerActionTypes = SetAllBeersAction
+interface SetMoreBeersAction {
+  type: typeof SET_MORE_BEERS,
+  beers: BeerItem[]
+}
+
+type BeerActionTypes = SetAllBeersAction | SetMoreBeersAction
 
 const setAllBeers = (beers: BeerItem[]): SetAllBeersAction => ({
   type: SET_ALL_BEERS,
   beers
 })
 
+const setMoreBeers = (beers: BeerItem[]): SetMoreBeersAction => ({
+  type: SET_MORE_BEERS,
+  beers
+})
+
 export const fetchAllBeers = () => {
   return async (dispatch: Dispatch) => {
     try {
-      const { data } = await axios.get('https://api.punkapi.com/v2/beers')
+      const { data } = await axios.get(`https://api.punkapi.com/v2/beers?page=1&per_page=${pagesPerPage.toString()}`)
       dispatch(setAllBeers(data))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+export const fetchMoreBeers = (pageNumber: number) => {
+  return async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios.get(`https://api.punkapi.com/v2/beers?page=${pageNumber.toString()}&per_page=${pagesPerPage.toString()}`)
+      dispatch(setMoreBeers(data))
     } catch (err) {
       console.error(err)
     }
@@ -32,6 +57,8 @@ const beerReducer = (state = beersReducerDefaultState, action: BeerActionTypes):
   switch (action.type) {
     case "SET_ALL_BEERS":
       return action.beers;
+    case "SET_MORE_BEERS":
+      return [...state, ...action.beers]
     default:
       return state;
   }
